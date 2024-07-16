bff71d6c5c2f0d7bcb874111ba7c1cd89f1f45be
unknown <shamanthanireddy07@gmail.com>
2024-07-16 15:58:16 -0400

    changes

---
+++ src/components/RegisterPage.js
@@ -0,0 +1,187 @@
+import React, { useState } from 'react';
+import { auth, db } from '../firebase'; // Import Firebase auth and database
+import { useNavigate, Link } from 'react-router-dom';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { setDoc, doc } from 'firebase/firestore';
+import '../css/RegisterPage.css';
+
+const RegistrationPage = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    firstName: '',
+    lastName: '',
+    mobile: '',
+    password: '',
+    confirmPassword: '',
+    address: ''
+  });
+  const [loading, setLoading] = useState(false);
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState('');
+  const navigate = useNavigate();
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validatePassword = (password) => {
+    return password.length >= 6;
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let valid = true;
+
+    if (!validateEmail(formData.email)) {
+      setEmailError('Invalid email address');
+      valid = false;
+    } else {
+      setEmailError('');
+    }
+
+    if (!validatePassword(formData.password)) {
+      setPasswordError('Password must be at least 6 characters');
+      valid = false;
+    } else {
+      setPasswordError('');
+    }
+
+    if (formData.password !== formData.confirmPassword) {
+      setConfirmPasswordError('Passwords do not match');
+      valid = false;
+    } else {
+      setConfirmPasswordError('');
+    }
+
+    if (!valid) return;
+
+    setLoading(true);
+    try {
+      
+      const userCredential = await createUserWithEmailAndPassword(auth, formData.email, formData.password);
+      const user = userCredential.user;
+
+      if (user) {
+        const { email, firstName, lastName, mobile, address } = formData;
+
+        await setDoc(doc(db, 'Users', user.uid), {
+          firstName,
+          lastName,
+          email,
+          mobile,
+          address,
+          isAdmin:false
+        });
+        alert('User registered successfully');
+        navigate('/login');
+      }
+    } catch (error) {
+      console.error('Error registering:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <div className="register-card">
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit} className="register-form">
+          <div className="input-group">
+            <label>First Name</label>
+            <input
+              type="text"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label>Last Name</label>
+            <input
+              type="text"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label>Email</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          {emailError && <p className="error">{emailError}</p>}
+          <div className="input-group">
+            <label>Mobile</label>
+            <input
+              type="text"
+              name="mobile"
+              value={formData.mobile}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label>Address</label>
+            <input
+              type="text"
+              name="address"
+              value={formData.address}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          {passwordError && <p className="error">{passwordError}</p>}
+          <div className="input-group">
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          {confirmPasswordError && <p className="error">{confirmPasswordError}</p>}
+          <button type="submit" disabled={loading}>
+            {loading ? 'Loading...' : 'Register'}
+          </button>
+          <div className="login-link">
+            <p>
+              Have an account? <Link to="/login">Login</Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default RegistrationPage;
